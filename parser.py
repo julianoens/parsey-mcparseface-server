07f69d109b5353985e174f46580e86d8cb0b7985
Juliano Efson Sales <julianoens@gmail.com>
2017-01-27 23:34:39 +0100

    defining base_dir as a param in server.py

--- parser.py
+++ parser.py
@@ -2,105 +2,124 @@
 
 from collections import OrderedDict
 import subprocess
+import os
 
-ROOT_DIR = "models/syntaxnet"
+#"/home/juliano/resources/"
+BASE_DIR = None
 PARSER_EVAL = "bazel-bin/syntaxnet/parser_eval"
 MODEL_DIR = "syntaxnet/models/parsey_mcparseface"
 
+
+def configure(base_dir):
+    global BASE_DIR
+    BASE_DIR = base_dir
+
+    global ROOT_DIR
+    ROOT_DIR = os.path.join(BASE_DIR, "models/syntaxnet")
+
+
 def open_parser_eval(args):
-  return subprocess.Popen(
-    [PARSER_EVAL] + args,
-    cwd=ROOT_DIR,
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE
-  )
+    if BASE_DIR is not None:
+        return subprocess.Popen(
+            [PARSER_EVAL] + args,
+            cwd=ROOT_DIR,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE
+        )
+
 
 def send_input(process, input):
-  process.stdin.write(input.encode("utf8"))
-  process.stdin.write(b"\n\n") # signal end of documents
-  process.stdin.flush()
-  response = b""
-  while True:
-    line = process.stdout.readline()
-    if line.strip() == b"":
-      # empty line signals end of response
-      break
-    response += line
-  return response.decode("utf8")
-
-# Open the part-of-speech tagger.
-pos_tagger = open_parser_eval([
-    "--input=stdin",
-    "--output=stdout-conll",
-    "--hidden_layer_sizes=64",
-    "--arg_prefix=brain_tagger",
-    "--graph_builder=structured",
-    "--task_context=" + MODEL_DIR + "/context.pbtxt",
-    "--model_path=" + MODEL_DIR + "/tagger-params",
-    "--slim_model",
-    "--batch_size=1024",
-    "--alsologtostderr",
-  ])
-
-# Open the syntactic dependency parser.
-dependency_parser = open_parser_eval([
-    "--input=stdin-conll",
-    "--output=stdout-conll",
-    "--hidden_layer_sizes=512,512",
-    "--arg_prefix=brain_parser",
-    "--graph_builder=structured",
-    "--task_context=" + MODEL_DIR + "/context.pbtxt",
-    "--model_path=" + MODEL_DIR + "/parser-params",
-    "--slim_model",
-    "--batch_size=1024",
-    "--alsologtostderr",
-  ])
+    process.stdin.write(input.encode("utf8"))
+    process.stdin.write(b"\n\n")  # signal end of documents
+    process.stdin.flush()
+    process.stdin.close()  # HERE
+    response = b""
+    while True:
+        line = process.stdout.readline()
+        if line.strip() == b"":
+            # empty line signals end of response
+            break
+        response += line
+    return response.decode("utf8")
 
 def split_tokens(parse):
-  # Format the result.
-  def format_token(line):
-    x = OrderedDict(zip(
-     ["index", "token", "unknown1", "label", "pos", "unknown2", "parent", "relation", "unknown3", "unknown4"],
-     line.split("\t")
-    ))
-    x["index"] = int(x["index"])
-    x["parent"] = int(x["parent"])
-    del x["unknown1"]
-    del x["unknown2"]
-    del x["unknown3"]
-    del x["unknown4"]
-    return x
-                                   
-  return [
-    format_token(line)
-    for line in parse.strip().split("\n")
-  ]
+    # Format the result.
+    def format_token(line):
+        x = OrderedDict(zip(
+            ["index", "token", "unknown1", "label", "pos", "unknown2", "parent", "relation", "unknown3", "unknown4"],
+            line.split("\t")
+        ))
+        x["index"] = int(x["index"])
+        x["parent"] = int(x["parent"])
+        del x["unknown1"]
+        del x["unknown2"]
+        del x["unknown3"]
+        del x["unknown4"]
+        return x
 
-def parse_sentence(sentence):
-  if "\n" in sentence or "\r" in sentence:
-    raise ValueError()
+    return [
+        format_token(line)
+        for line in parse.strip().split("\n")
+        ]
 
-  # Do POS tagging.
-  pos_tags = send_input(pos_tagger, sentence + "\n")
+def parse_sentence(sentence, tags, base_dir):
+    configure(base_dir)
+    # Open the part-of-speech tagger.
+    pos_tagger = open_parser_eval([
+        "--input=stdin",
+        "--output=stdout-conll",
+        "--hidden_layer_sizes=64",
+        "--arg_prefix=brain_tagger",
+        "--graph_builder=structured",
+        "--task_context=" + MODEL_DIR + "/context.pbtxt",
+        "--model_path=" + MODEL_DIR + "/tagger-params",
+        "--slim_model",
+        "--batch_size=1024",
+        "--alsologtostderr",
+    ])
 
-  # Do syntax parsing.
-  dependency_parse = send_input(dependency_parser, pos_tags)
+    # Open the syntactic dependency parser.
+    dependency_parser = open_parser_eval([
+        "--input=stdin-conll",
+        "--output=stdout-conll",
+        "--hidden_layer_sizes=512,512",
+        "--arg_prefix=brain_parser",
+        "--graph_builder=structured",
+        "--task_context=" + MODEL_DIR + "/context.pbtxt",
+        "--model_path=" + MODEL_DIR + "/parser-params",
+        "--slim_model",
+        "--batch_size=1024",
+        "--alsologtostderr",
+    ])
+    if "\n" in sentence or "\r" in sentence:
+        raise ValueError()
 
-  # Make a tree.
-  dependency_parse = split_tokens(dependency_parse)
-  tokens = { tok["index"]: tok for tok in dependency_parse }
-  tokens[0] = OrderedDict([ ("sentence", sentence) ])
-  for tok in dependency_parse:
-     tokens[tok['parent']]\
-       .setdefault('tree', OrderedDict()) \
-       .setdefault(tok['relation'], []) \
-       .append(tok)
-     del tok['parent']
-     del tok['relation']
+    if sentence is not None:
+    # Do POS tagging.
+        pos_tags = send_input(pos_tagger, sentence + "\n")
+    else:
+        pos_tags = tags
 
-  return tokens[0]
+    # Do syntax parsing.
+    dependency_parse = send_input(dependency_parser, pos_tags)
+
+    # Make a tree.
+    dependency_parse = split_tokens(dependency_parse)
+    tokens = {tok["index"]: tok for tok in dependency_parse}
+    tokens[0] = OrderedDict([("sentence", sentence)])
+    for tok in dependency_parse:
+        tokens[tok['parent']] \
+            .setdefault('tree', OrderedDict()) \
+            .setdefault(tok['relation'], []) \
+            .append(tok)
+        del tok['parent']
+        del tok['relation']
+
+    return tokens[0]
 
 
 if __name__ == "__main__":
-  import sys, pprint
-  pprint.pprint(parse_sentence(sys.stdin.read().strip())["tree"])
+    import sys, pprint
+
+    # pprint.pprint(parse_sentence(sys.stdin.read().strip())["tree"])
+    pprint.pprint(parse_sentence("Translate my CV from English to Portuguese"))
--- server.py
+++ server.py
@@ -4,26 +4,39 @@
 # sudo pip3 install flask
 
 import os
+import sys
+
 from flask import Flask, request, Response
 from multiprocessing import Pool
-from parser import parse_sentence
 import json
 
+import parser
+
+base_dir = None
 app = Flask(__name__)
 port = 80 if os.getuid() == 0 else 8000
 
 pool = Pool(1, maxtasksperchild=50)
 
-@app.route('/')
-def index():
-  q = request.args.get("q", "")
-  result = pool.apply(parse_sentence, [q])
 
-  return Response(
-    response=json.dumps(result, indent=2),
-    status=200,
-    content_type="application/json")
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    tags = None
+
+    if request.method == 'GET':
+        sentence = request.args.get("sentence", "")
+        if sentence is None:
+            tags = request.args.get("tags", "")
+    else:
+        sentence = request.get_json()['q']
+    result = pool.apply(parser.parse_sentence, [sentence, tags, base_dir])
+
+    return Response(
+        response=json.dumps(result, indent=2),
+        status=200,
+        content_type="application/json")
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=port, host="0.0.0.0")
-
+    base_dir = sys.argv[1]
+    app.run(debug=False, port=port, host="0.0.0.0")
